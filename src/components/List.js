8211b301e502398b83e08f51dc30ab2e8702476d
Dima <respect4me@ukr.net>
2021-08-06 21:23:08 +0300

    test push

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,13 @@
-import { useEffect } from "react"
+
 import { useSelector } from "react-redux"
 
 export const List =()=>{
     const stateList = useSelector(state => state.reducer)
-    useEffect(() => {
-        setTimeout(console.log(stateList),500)
-   
-    }, [stateList]) 
+
    if(stateList){ 
 
-       return(<ul>{stateList.map((item,key)=><li className={`field-${key+1}`}><i>{item.date}</i><b>{item.text}</b><i>{item.time}</i></li>)}</ul>) 
-}else{
-     return (<div>Nothing</div>)
-}
+    return(<ul>{stateList.map((item,key)=><li className={`field-${key+1}`}><i>{item.date}</i><b>{item.text}</b><i>{item.time}</i></li>)}</ul>) 
+                }else{
+    return (<div>Nothing</div>)
+}   
 }
